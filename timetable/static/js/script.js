567f1898bda4e69e96495f4cc7d299f68858b6e1
Parzivald <parzivald@inbox.ru>
2022-12-14 13:08:07 +0500

    final

---
+++ timetable/static/js/script.js
@@ -0,0 +1,27 @@
+// /*
+// пустой выпадающий список при загрузке страницы
+// document.getElementById("TeachersDropdown").selectedIndex = -1;*/
+
+// function myDate() {
+//     var date = new Date(),
+//     targetDay = 5, // пятница, начиная с вс=0
+//     targetDate = new Date(),
+//     delta = targetDay - date.getDay();
+//     if (delta >= 0) {targetDate.setDate(date.getDate() + delta)}
+//     else {targetDate.setDate(date.getDate() + 7 + delta)}
+//     alert(targetDate);
+//     a = 5
+//     console.log(targetDate);
+// }
+
+
+// function getLessonListHTML(orphan) {
+//     return
+//     `
+
+//     `
+//     document.getElementById('body').innerHTML = List
+// }
+
+
+// if MondayDate == lesson.date & MondayTime == lesson.time
